21d37d53f44117bdb70c10db522c3b1010489092
ZoeKIM752 <40921231+ZoeKIM752@users.noreply.github.com>
2022-10-26 23:40:43 +0900

    make UserService

--- PNU/src/main/java/pnu/user/service/impl/UserServiceImpl.java
+++ PNU/src/main/java/pnu/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,22 @@
 package pnu.user.service.impl;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import pnu.user.dao.UserDAO;
+import pnu.user.service.UserService;
+
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserDAO userDAO;
+
+	@Override
+	public Boolean selectPwd(String id, String password) {
+		if(password.equals(userDAO.selectPwd(id))){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
