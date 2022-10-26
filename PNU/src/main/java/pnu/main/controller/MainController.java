af4cc9b3ee7306d90f88778d5a0f85fd0c0e5e1e
ZoeKIM752 <40921231+ZoeKIM752@users.noreply.github.com>
2022-10-26 23:48:35 +0900

    make login controller

--- PNU/src/main/java/pnu/main/controller/MainController.java
+++ PNU/src/main/java/pnu/main/controller/MainController.java
@@ -1,14 +1,31 @@
 package pnu.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pnu.user.service.UserService;
+import pnu.user.vo.UserVO;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+	private UserService userService;
 
-	@RequestMapping(value="/main.do", method = RequestMethod.GET)
-	public String mainPage() {
-		return "main.html";
+	@RequestMapping(value="/login.do", method = RequestMethod.POST)
+	public String mainPage(@ModelAttribute UserVO user) {
+		if(userService.selectPwd(user.getId(), user.getPwd())){
+			return "main.jsp";
+		} else {
+			return "login.jsp";
+		}
+	}
+	
+	@RequestMapping("/loginPage.do")
+	public String loginPage() {
+		return "login.jsp";
 	}
 }
