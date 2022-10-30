210c18f4e635bea2f8b08ea51a93a1d562f318c0
ZoeKIM752 <40921231+ZoeKIM752@users.noreply.github.com>
2022-10-30 19:35:18 +0900

    make FE

---
+++ PNU/src/main/java/pnu/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package pnu.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/signUpPage.do")
+	public String signUpPage() {
+		return "user/signUp.jsp";
+	}
+	
+}
