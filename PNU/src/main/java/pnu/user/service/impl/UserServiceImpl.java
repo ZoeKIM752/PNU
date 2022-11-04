6fad697bc74a0657f127a3783a19c9e572ef6e4d
ZoeKIM752 <40921231+ZoeKIM752@users.noreply.github.com>
2022-11-04 18:41:57 +0900

    modify day3

--- PNU/src/main/java/pnu/user/service/impl/UserServiceImpl.java
+++ PNU/src/main/java/pnu/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,20 @@
 package pnu.user.service.impl;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pnu.user.dao.UserDAO;
+import pnu.user.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	UserDAO userDAO;
+	
+	@Override
+	public Boolean selectPwd(String userId, String pwd) {
+		return pwd.equals(userDAO.selectPwd(userId));
+	}
 
 }
